d57c32568c9deaa69aa61e953979708a1f52c7f2
luquilo <luquilo.etc@gmail.com>
2023-04-06 18:20:50 +0700

    menambahkan babel loader

--- webpack.common.js
+++ webpack.common.js
@@ -41,6 +41,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ]
+      }
     ],
   },
   /* plugin */
